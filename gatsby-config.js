5703bbe92a5ac177cf4acd01ce496e4897582559
Louis Sandoval <louis@sandoval.com>
2019-08-20 20:36:31 -0700

    Add categories for different roles on homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Overwatch Party`,
+    description: `Ain't no party like an Overwatch party`,
+    author: `OverwatchFam`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
